d6fbfb21817aab7f2c421d70ae4ee9bedfa7095e
IurySantoss <86297297+IurySantoss@users.noreply.github.com>
2022-11-01 11:31:37 -0300

    Add files via upload

--- php/banco.php
+++ php/banco.php
@@ -2,13 +2,14 @@
     Class Conexao {
         private $pdo;
 
-        public function conectar ($bd, $host, $user,$senha){
-            try {
+       public function conectar ($bd, $host, $user,$senha){
+           try {
                 $pdo = new PDO ('mysql:host=' .$host. ';dbname=' .$bd,$user,$senha);
-            } catch (PDOException $erro){
-                die ("Erro .<code>" . $erro->getMessage() . "</code>");
-            }
+           } catch (PDOException $erro){
+               die ("Erro .<code>" . $erro->getMessage() . "</code>");
+           }
             return ($pdo);
         }
-    }
+  }
+
 ?>
